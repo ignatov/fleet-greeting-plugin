ecf37c1b4ee86d1fc16f2b8ea7569b1ce402d2ad
Sergey Ignatov <sergey.ignatov@jetbrains.com>
2023-01-05 23:04:45 +0100

    use fleet version

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,34 +4,18 @@ plugins {
     id("org.jetbrains.fleet") version "0.2-SNAPSHOT"
 }
 
-//val path = "/Users/ignatov/Library/Application Support/JetBrains/Toolbox/apps/Fleet/ch-0/1.13.76" +
-//        "/Fleet.app/"
-
 fleet {
-    fleetPath.set("/Users/ignatov/Fleet/32261aa6f5d60-debug/Fleet.app/")
+//    fleetPath.set("/Users/ignatov/Fleet/32261aa6f5d60-debug/Fleet.app/")
+    fleetVersion.set("1.14.24")
 
 //    fleetPlugins {
 //
 //    }
-//
-//    frontend {
-//        dependencies {
-//
-//        }
-//    }
-//
-//    workspace {
-//        dependencies {
-//
-//        }
-//    }
+    frontend {
+    }
+    workspace {
+    }
 }
-
-//tasks.withType<KotlinCompile> {
-//    kotlinOptions.jvmTarget = "17"
-//    kotlinOptions.apiVersion = "1.7"
-//    kotlinOptions.languageVersion = "1.7"
-//}
 
 repositories {
     jcenter()
@@ -39,5 +23,3 @@ repositories {
     mavenCentral()
     maven("https://cache-redirector.jetbrains.com/intellij-dependencies")
 }
-
-
