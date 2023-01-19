acfc292539949aca0631bceac6d6c0cbdacdb238
Sergey Ignatov <sergey.ignatov@jetbrains.com>
2023-01-19 14:59:22 +0100

    kotlin 1.8 + extra libs

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,20 +1,25 @@
 plugins {
-    kotlin("jvm") version "1.7.22"
+    kotlin("jvm") version "1.8.0"
     `kotlin-dsl`
     id("org.jetbrains.fleet") version "0.2-SNAPSHOT"
 }
 
 fleet {
-//    fleetPath.set("/Users/ignatov/Fleet/32261aa6f5d60-debug/Fleet.app/")
-    fleetVersion.set("1.14.24")
+//    fleetPath.set("/Users/ignatov/Fleet/87120b1bf721e-debug/Fleet.app")
+    fleetVersion.set("1.15.6")
 
-//    fleetPlugins {
-//
-//    }
-    frontend {
-    }
     workspace {
     }
+
+    frontend {
+        add("org.twitter4j:twitter4j-core:4.1.1")
+    }
+
+    common {
+        add("org.apache.samza:samza-shell:0.13.1")
+    }
+
+    plugins.addAll("fleet.run")
 }
 
 repositories {
