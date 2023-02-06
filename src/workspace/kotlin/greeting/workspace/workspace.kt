88cd955b93e0dd62ac38b0b601ec42c21246a2cd
Sergey Ignatov <sergey.ignatov@jetbrains.com>
2023-02-06 15:08:41 +0100

    use ktor in the example

--- src/workspace/kotlin/greeting/workspace/workspace.kt
+++ src/workspace/kotlin/greeting/workspace/workspace.kt
@@ -2,7 +2,12 @@ package greeting.workspace
 
 import com.jetbrains.rhizomedb.Entrypoint
 import fleet.kernel.ChangeScope
+import fleet.kernel.saga
 import greeting.common.ExampleSharedEntity
+import io.ktor.client.*
+import io.ktor.client.engine.cio.*
+import io.ktor.client.request.*
+import io.ktor.client.statement.*
 
 @Entrypoint
 fun ChangeScope.entry() {
@@ -14,6 +19,13 @@ fun ChangeScope.entry() {
     shared {
         new(ExampleSharedEntity::class) {
             name = "example"
+        }
+    }
+
+    kernel.saga {
+        HttpClient(CIO).use { client ->
+            val response: HttpResponse = client.get("https://ktor.io/")
+            println("KTOR response: " + response.status)
         }
     }
 }
--- src/workspace/kotlin/module-info.java
+++ src/workspace/kotlin/module-info.java
@@ -3,6 +3,9 @@ module fleet.greeting.plugin.workspace {
     requires fleet.kernel;
     requires fleet.greeting.plugin.common;
     requires fleet.run.common;
+    requires io.ktor.client.core;
+    requires io.ktor.client.cio;
+    requires kotlinx.coroutines.core.jvm;
 
     exports greeting.workspace;
 }
